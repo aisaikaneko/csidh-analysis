de0364dd1c7740616b863c8ae2c5dde31ecb03f4
Harrison Lemley <objective.axioma@gmail.com>
2024-09-08 21:34:44 -0400

    Many issues remain, but the program is done. Now debugging.

--- csidh.py
+++ csidh.py
@@ -1,22 +1,6 @@
 from sage.all import *
 import random as rand
 
-# Converts a curve of the form y^2 = x^3 + Ax^2 + x to Weierstrauss form
-def convert_to_weierstrauss(A):
-    a = (3 - A**2) * pow(3 * 1, -1, p)
-    b = (2 * A**3 - 9*A) * pow(27 * 1, -1, p)
-    return EllipticCurve(F, [a, b])
-
-# Converts a curve from Weierstrauss form back to the form y^2 + x^3 + Ax^2 + x
-def convert_from_weierstrauss(E):
-    a = E.a4()
-    b = E.a6()
-    R = PolynomialRing(F, name="z")
-    roots = (z**3 + z*z + b).roots()
-    r = roots[0][0]
-    s = (3*s**2 + a).sqrt() ** (-1)
-    return -3 * (-1)**s.is_square() * r * s
-
 # Generate the parameters l_primes, p, and F_p for the key exchange
 def gen_params(n):
     l_primes = primes_first_n(n + 1)[1:]
@@ -29,6 +13,24 @@ def gen_params(n):
     F = GF(p)
     return l_primes, p, F
 
+# Converts a curve of the form y^2 = x^3 + Ax^2 + x to Weierstrauss form
+def convert_to_weierstrauss(A):
+    a = (3 - A**2) * pow(3 * 1, -1, p)
+    b = (2 * A**3 - 9*A) * pow(27 * 1, -1, p)
+    return EllipticCurve(F, [a, b])
+
+# Converts a curve from Weierstrauss form back to the form y^2 + x^3 + Ax^2 + x
+def convert_from_weierstrauss(E):
+    a = E.a4()
+    b = E.a6()
+    R = PolynomialRing(F, name="z")
+    z = R.gens()[0]
+    roots = (z**3 + z**2 + b).roots()
+    assert len(roots) > 0
+    r = roots[0][0]
+    s = (3*r**2 + a).sqrt() ** (-1)
+    return -3 * (-1)**s.is_square() * r * s
+
 # Generate the private and public keys for the key exchange
 def gen_key(self, curve, m):
     return {
@@ -40,5 +42,49 @@ def gen_key(self, curve, m):
 def group_action(key):
     e_list = key["private"]
     A = key["public"]
-    E = get_curve(A)
+    E = convert_to_weierstrauss(A)
+    
+    for e in e_list:
+        if e != 0:
+            S = []
+            while S == []:
+                x = F.random_element()
+                r = F(x**3 + A*x**2 + x)
+                s = kronecker_symbol(r, p)
+                for i in range(len(e_list)):
+                    if sign(e_list[i]) == s:
+                        S.append(i)
+            if s == -1:
+                E = E.quadratic_twist()
+
+            x = E.random_element()
+            while x.is_zero():
+                x = E.random_element()
+            x = x.xy()[0]
+            k = prod([l_primes[i] for i in S])
+            P = E.lift_x(x)
+            Q = ((p+1)//k) * P
+            for i in S:
+                R = (k // l_primes[i]) * Q
+                if R.is_zero():
+                    continue
+                phi = E.isogeny(R)
+                E = phi.codomain()
+                Q = phi(Q)
+                k = k // l_primes[i]
+                e_list[i] -= s
+            if s == -1:
+                E = E.quadratic_twist()
+    return convert_from_weierstrauss(E)
+
+for _ in range(100):
+    l_primes, p, F = gen_params(3)
+    alice_priv = [randrange(-5, 6) for _ in range(len(l_primes))]
+    bob_priv = [randrange(-5, 6) for _ in range(len(l_primes))]
+    alice_pub = group_action({"public": 0, "private": alice_priv})
+    bob_pub = group_action({"public": 0, "private": bob_priv})
+    alice_shared = group_action({"public": bob_pub, "private": alice_priv})
+    bob_shared = group_action({"public": alice_pub, "private": bob_priv})
+    print(alice_shared == bob_shared)
+
 
