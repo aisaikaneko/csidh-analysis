646a302d4d79b0f486d180d33b4436d6c10b9c26
Crypto-Neko <objective.axioma@gmail.com>
2024-09-17 21:07:45 -0400

    Improved comments.

--- csidh.py
+++ csidh.py
@@ -29,13 +29,13 @@ class CSIDH():
             "public": public
         }
 
-    # Converts a curve of the form y^2 = x^3 + Ax^2 + x to Weierstrauss form
+    # Converts a curve of the form y^2 = x^3 + Ax^2 + x to simplified Weierstrauss form
     def convert_to_weierstrauss(self, A):
         a = (3 - A**2) * pow(3 * 1, -1, self.p)
         b = (2 * A**3 - 9*A) * pow(27 * 1, -1, self.p)
         return EllipticCurve(self.F, [a, b])
 
-    # Converts a curve from Weierstrauss form back to A from the form y^2 = x^3 + Ax^2 + x
+    # Converts a curve from simplified Weierstrauss form back to A from the form y^2 = x^3 + Ax^2 + x
     def convert_from_weierstrauss(self, E):
         a = E.a4()
         b = E.a6()
@@ -51,32 +51,42 @@ class CSIDH():
     # Apply the CSIDH group action
     def group_action(self, key):
         # Set up parameters
-        e_list = key["private"].copy()
-        A = key["public"]
+        e_list = key["private"].copy()              # List of exponents of prime ideals
+        A = key["public"]                           # Coefficient for elliptic curve
         E = self.convert_to_weierstrauss(A)
-        p = self.p
-        F = self.F
-        l_primes = self.l_primes
+        p = self.p                                  # The prime to use for the group acton
+        F = self.F                                  # Prime field with orer p
+        l_primes = self.l_primes                    # List of small primes
         
         # Return the base curve if each e_i = 0
         if all(e == 0 for e in e_list):
             return A
 
         # Apply the ideal corresponding to each prime l_i and exponent e_i
+        # Uses a loop structure to apply each idela the right number of times until no more exponents remain
         while True:
+            # If no exponent is positive, then end and return E
             if all(e == 0 for e in e_list):
                 break
+
+            # Get a non-zero random element of F
             x = F.random_element()
             while x.is_zero():
                 x = F.random_element()
+
+            # Set s to the Kronecker symbol of r for p
             r = F(x**3 + A*x**2 + x)
             s = kronecker_symbol(r, p)
             assert (2 * is_square(r)) - 1 == s
+
+            # Create a set of all non-zero indices
             S = [i for i, e in enumerate(e_list) if sign(e) == s]
             if len(S) == 0:
                 continue
             if s == -1:
                 E = E.quadratic_twist()
+            
+            # Get a random point on the curve and get the product of all elements
             while True:
                 y = E.random_element()
                 if not y.is_zero():
@@ -84,8 +94,12 @@ class CSIDH():
             x = y.xy()[0]
             k = prod(l_primes[i] for i in S)
             P = E.lift_x(x)
+
+            # Ensure that p + 1 divides k and define Q = ((p + 1) / k) * P
             assert (p + 1) % k == 0
             Q = ((p + 1) // k) * P
+
+            # Apply the isogeny corresponding to each l_prime power to compute E_B
             for i in S:
                 assert k % l_primes[i] == 0
                 R = (k // l_primes[i]) * Q
